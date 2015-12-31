d7ea54fdf23881553d38a8a5d91f28664002e21f
luisangonzalez <luisantonio.gonzalez@bq.com>
2015-12-31 04:05:41 +0100

    first completly first homework --> Introduction to MongoDB and Node.js

---
+++ chapterHomework/interface.js
@@ -0,0 +1,30 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+   db.collection('movies').insert(doc, function(error, result) {
+      if (error) {
+         console.log(error);
+         process.exit(1);
+      }
+      callback(result);
+      process.exit(0);
+   });
+   callback(null);
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+   db.collection('movies').find({
+      'director': director
+   }).sort({
+      title: 1
+   }).toArray(function(err, docs) {
+      callback(null, docs);
+   });
+};
